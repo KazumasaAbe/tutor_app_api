59149ec50ad265fb417c22f5c23c598b52524c30
AbeKazumasa <katahira0088@gmail.com>
2022-01-02 22:46:22 +0900

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :students
+  devise_for :teachers
 
   namespace :api do
     namespace :v1 do
       resources :hello, only:[:index]
       mount_devise_token_auth_for 'Student', at: 'student'
-    end
+      mount_devise_token_auth_for 'Teacher', at: 'teacher'
+      end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 Student.create(email: 'admin@example.com',
-  password: 'password')
+               password: 'password',
+               admin: false,
+               teacher: false)
+
+Teacher.create(email: 'admin@example.com',
+               password: 'password',
+               teacher: true,
+               admin: false)
