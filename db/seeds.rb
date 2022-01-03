4ec53ae3d6f20293de6eb8ebd3ba5b572d6e96b1
AbeKazumasa <katahira0088@gmail.com>
2022-01-03 21:25:11 +0900

    login完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-Student.create(email: 'admin@example.com',
-               password: 'password',
-               admin: false,
-               teacher: false)
-
 Teacher.create(email: 'admin@example.com',
                password: 'password',
                teacher: true,
-               admin: false)
+               admin: true)
+
+10.times do |n|
+  email = "student-#{n+1}@email.com"
+  password = "password"
+  Student.create!(email: email,
+                  password: password,
+                  password_confirmation: password,
+                  teacher: false)
+end
+
+10.times do |n|
+  email = "teacher-#{n+1}@email.com"
+  password = "password"
+  Teacher.create!(email: email,
+                  password: password,
+                  password_confirmation: password,
+                  teacher: true)
+end
