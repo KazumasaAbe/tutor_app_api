2c0eb8a34c9bb1305229453b6a94a6f0a9194f9f
kwansai724 <kwansai.taka724@gmail.com>
2022-01-22 00:44:41 +0900

    seedファイル一部変調整

--- db/seeds/development/teachers.rb
+++ db/seeds/development/teachers.rb
@@ -1,17 +1,15 @@
 10.times do |n|
-  name = "teacher-#{n}"
-  email = "#{name}@example.com"
-  introduction = "初めまして！teacher-#{n}といいます。
-                  生徒に合わせ教え方を変えて勉強が楽しい！と
-                  思ってもらえるように笑顔を絶やさず丁寧に教えます！"
+  name = "先生#{n}"
+  email = "teacher-#{n+1}@email.com"
+  introduction = "初めまして！先生#{n}といいます。生徒に合わせ教え方を変えて勉強が楽しい！と思ってもらえるように笑顔を絶やさず丁寧に教えます！"
   password = "password"
-  Teacher.create!(
-    name: name,
-    email: email,
-    introduction: introduction,
-    password: password
-  )
-end 
+  Teacher.create!(name: name,
+                  email: email,
+                  introduction: introduction,
+                  password: password,
+                  password_confirmation: password,
+                  teacher: true)
+end
 
 Teacher.create(email: 'admin@example.com',
   password: 'password',
