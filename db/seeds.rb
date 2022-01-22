baa3217e3786e1f9d0f6788d21a8cd96b412c0d2
AbeKazumasa <katahira0088@gmail.com>
2022-01-22 23:03:17 +0900

    marge槇さん

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,40 +22,6 @@ table_names.each do |table_name|
   require path
  end
 
-Teacher.create(email: 'admin@example.com',
-               password: 'password',
-               teacher: true,
-               admin: true)
-
-10.times do |n|
-  email = "student-#{n+1}@email.com"
-  password = "password"
-  Student.create!(email: email,
-                  password: password,
-                  password_confirmation: password,
-                  teacher: false)
-end
-
-10.times do |n|
-  name = "teacher-#{n}"
-  email = "teacher-#{n+1}@email.com"
-  introduction = "初めまして！teacher-#{n}といいます。生徒に合わせ教え方を変えて勉強が楽しい！と思ってもらえるように笑顔を絶やさず丁寧に教えます！"
-  password = "password"
-  Teacher.create!(name: name,
-                  email: email,
-                  introduction: introduction,
-                  password: password,
-                  password_confirmation: password,
-                  teacher: true)
-end
-
-10.times do |n|
-  subject = "英語"
-  teacher_id = n+1
-  Subject.create!(subject: subject,
-                  teacher_id: teacher_id)
-end
-
 
 Notice.create!(title: '新しい先生が加わりました!', text: '2021年より新しい先生が加わります!!教科は、「国語」「理科」を担当してもらいます。')
 Notice.create!(title: '新しい先生が加わりました!', text: '2021年より新しい先生が加わります!!教科は、「国語」「理科」を担当してもらいます。')
