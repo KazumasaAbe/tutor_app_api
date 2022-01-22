41bf601ce7597feb7b806974b899384e013e0ba9
kwansai724 <kwansai.taka724@gmail.com>
2022-01-22 22:45:04 +0900

    teacherコントローラー修正途中

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::TeachersController < ApplicationController
 
   def teacher_index
-    teachers = Teacher.where(admin: nil).joins(:subjects).select(:id, :name, :email, :introduction, :teacher_icon, :subject)
+    teachers = Teacher.where(admin: nil).joins(:subjects).select(:id, :name, :email, :introduction, :teacher_icon, :subject).order(:id)
     render json: teachers, status: 200
   end
 
   def update
     teacher = Teacher.find(params[:id])
+    # subject = Subject.find_by(params[:teacher_id])
+    # if subject.update(subject_params) && teacher.update(teacher_params)
     if teacher.update(teacher_params)
       render json: teacher, status: 200
     else
@@ -28,5 +30,9 @@ class Api::V1::TeachersController < ApplicationController
     def teacher_params
       params.require(:teacher).permit(:id, :name, :email, :introduction, :teacher_icon, subjects_attributes: [:id, :subject, :_destroy])
     end
+
+    # def subject_params
+    #   params.permit(:id, :subject)
+    # end
 
 end
