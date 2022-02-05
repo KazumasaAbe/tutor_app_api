21d48c185c0f76d8e887d4035765277dd8926457
ahimi <testtestabe07@gmail.com>
2022-02-05 22:02:45 +0900

    teacher-config

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -1,14 +1,48 @@
 class Api::V1::TeachersController < ApplicationController
   
   def teacher_index
-    teachers = Teacher.where(admin: nil).joins(:subjects).select(:id, :name, :email, :introduction, :teacher_icon, :subject).order(:id)
-    render json: teachers, status: 200
+    # teachers = Teacher.where(admin: nil).joins(:subjects).select(:id, :name, :email, :introduction, :teacher_icon, :subject).order(:id)
+    teachers = Teacher.where(admin: nil).eager_load(:subjects).order(:id)
+    # subjects = Subject.all 
+    # @teachers = Teacher.includes(:subjects)
+    # teachers = teachers.subjects.build
+    # render json: teachers, subjects, status: 200
+    # render json:  teachers, status: 200
+    render json: teachers.to_json(include:[:subjects])
   end
 
   def update
     teacher = Teacher.find(params[:id])
     subject = Subject.find(params[:id])
-    if subject.update(subject_params) && teacher.update(teacher_params)
+    # if subject.update(subject_params) && teacher.update(teacher_params)
+    # debugger
+    # if params[:subjects][:subject] && teacher.update(teacher_params)
+    # debugger
+    # subject_params.each do |subject|
+    #   subject.update_attributes!
+    # end
+    if teacher.update(teacher_params) && subject.update(subject_params)
+    # if teacher.update(teacher_params)
+    #   params[:subjects][:subject].each do |item|
+    #     # debugger
+    #     subjects=Subject.where(teacher_id: params[:id])
+    #     subjects.each do |subject|
+    #     # debugger
+    #       if subject.subject == item
+    #         subject.update(item)
+    #       else
+    #         subject.save(item)
+    #       end
+    #     end
+    #     # subject.update_attributes!
+    #   end
+    # end
+    # teacher_params.each do |id, item|
+    # subject_params.each do |id, item|
+      # teacher = Teacher.find(id)
+      # teacher.update_attributes!(item)
+    # end
+    # if teacher.update(teacher_params)
       render json: teacher, status: 200
     else
       render json: teacher, status: 500
@@ -27,11 +61,12 @@ class Api::V1::TeachersController < ApplicationController
   private
 
     def teacher_params
-      params.require(:teacher).permit(:id, :name, :email, :introduction, :teacher_icon)
+      params.permit(:id, :name, :email, :introduction, :teacher_icon)
+      # params.require(:teacher).permit(:id, :name, :email, :introduction, :teacher_icon, subjects_attributes:[:subject])
     end
 
     def subject_params
-      params.permit(:subject)
+      params.require(:subjects).permit({ subject: [] })
     end
 
 end
