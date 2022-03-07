2c5ca566fd807c28d63ede61e3d8b417f51ccdba
ahimi <testtestabe07@gmail.com>
2022-03-07 15:59:00 +0900

    notice画像保存

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,2 +1,10 @@
-class Notice < ApplicationRecord
+# class Notice < ApplicationRecord
+class Notice < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
+  
+  has_one_attached :notice_icon
+
+  def notice_icon_url
+    notice_icon.attached? ? url_for(notice_icon) : nil
+  end
 end
